0166a9cb3f8e18e1546af3bed9f9c200f94ab179
Jack Heintz <jheintz222@gmail.com>
2024-11-22 21:01:34 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,386 @@
+#include <stdio.h>
+#include <windows.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <math.h>
+#include <string.h>
+#include "surfaceModeller.h"
+#include <iostream>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+GLdouble worldLeft = -12;
+GLdouble worldRight = 12;
+GLdouble worldBottom = -9;
+GLdouble worldTop = 9;
+GLdouble worldCenterX = 0.0;
+GLdouble worldCenterY = 0.0;
+GLdouble wvLeft = -12;
+GLdouble wvRight = 12;
+GLdouble wvBottom = -9;
+GLdouble wvTop = 9;
+
+GLint glutWindowWidth = 800;
+GLint glutWindowHeight = 600;
+GLint viewportWidth = glutWindowWidth;
+GLint viewportHeight = glutWindowHeight;
+
+// screen window identifiers
+int window3D;
+
+int window3DSizeX = 800, window3DSizeY = 600;
+GLdouble aspect = (GLdouble)window3DSizeX / window3DSizeY;
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, (char**)argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(glutWindowWidth, glutWindowHeight);
+	glutInitWindowPosition(50, 100);
+
+	// The 3D Window
+	window3D = glutCreateWindow("Surface of Revolution");
+	glutPositionWindow(900, 100);
+	glewInit();
+	glutDisplayFunc(display3D);
+	glutReshapeFunc(reshape3D);
+	glutMouseWheelFunc(mouseScrollWheelHandler3D);
+	glutMotionFunc(mouseMotionHandler3D);
+	glutKeyboardFunc(keyboardHandler3D);
+	// Initialize the 3D system
+	init3DSurfaceWindow();
+	// Annnd... ACTION!!
+	glutMainLoop();
+
+	return 0;
+}
+
+// Ground Mesh material
+GLfloat groundMat_ambient[] = { 0.4, 0.4, 0.4, 1.0 };
+GLfloat groundMat_specular[] = { 0.01, 0.01, 0.01, 1.0 };
+GLfloat groundMat_diffuse[] = { 0.4, 0.4, 0.7, 1.0 };
+GLfloat groundMat_shininess[] = { 1.0 };
+
+GLfloat light_position0[] = { 4.0, 8.0, 8.0, 1.0 };
+GLfloat light_diffuse0[] = { 1.0, 1.0, 1.0, 1.0 };
+
+GLfloat light_position1[] = { -4.0, 8.0, 8.0, 1.0 };
+GLfloat light_diffuse1[] = { 1.0, 1.0, 1.0, 1.0 };
+
+GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat model_ambient[] = { 0.5, 0.5, 0.5, 1.0 };
+
+//
+// Surface of Revolution consists of vertices and quads
+//
+// Set up lighting/shading and material properties for surface of revolution
+GLfloat quadMat_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat quadMat_specular[] = { 0.45, 0.55, 0.45, 1.0 };
+GLfloat quadMat_diffuse[] = { 0.1, 0.35, 0.1, 1.0 };
+GLfloat quadMat_shininess[] = { 10.0 };
+
+
+// Quads and Vertices of the surface of revolution
+typedef struct Vertex
+{
+	GLdouble x, y, z;
+	Vector3D normal;
+} Vertex;
+
+class Mesh {
+public:
+	// mesh data
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices)
+	{
+		this->vertices = vertices;
+		this->indices = indices;
+
+		setupMesh();
+	}
+	void setupMesh()
+	{
+		glGenVertexArrays(1, &VAOid);
+		glGenBuffers(1, &VBOid);
+		glGenBuffers(1, &IBOid);
+
+		glBindVertexArray(VAOid);
+
+		// vertice buffer
+		glBindBuffer(GL_ARRAY_BUFFER, VBOid);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+		// index buffer
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBOid);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+		// vertex positions
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, sizeof(Vertex), (void*)0);
+
+		// vertex normals
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+
+		// unbind buffers
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+
+	// Render method
+	void Draw() {
+		glBindVertexArray(VAOid);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	}
+private:
+	unsigned int VAOid, VBOid, IBOid;
+};
+
+unsigned int VAOid;
+unsigned int VBOid;
+unsigned int IBOid;
+
+GLdouble fov = 60.0;
+
+int lastMouseX;
+int lastMouseY;
+
+GLdouble eyeX = 0.0, eyeY = 3.0, eyeZ = 10.0;
+GLdouble radius = eyeZ;
+GLdouble zNear = 0.1, zFar = 40.0;
+
+Assimp::Importer importer;
+
+void loadModel();
+Mesh* loadedMesh = nullptr;
+
+void init3DSurfaceWindow()
+{
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse0);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position0);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, model_ambient);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse1);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT1, GL_AMBIENT, model_ambient);
+	glLightfv(GL_LIGHT1, GL_POSITION, light_position1);
+
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_NORMALIZE);    // Renormalize normal vectors 
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
+
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LINE_SMOOTH);
+	glClearColor(0.4F, 0.4F, 0.4F, 0.0F);  // Color and depth for glClear
+
+	glViewport(0, 0, (GLsizei)window3DSizeX, (GLsizei)window3DSizeY);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(fov, aspect, zNear, zFar);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(eyeX, eyeY, eyeZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+
+void reshape3D(int w, int h)
+{
+	glutWindowWidth = (GLsizei)w;
+	glutWindowHeight = (GLsizei)h;
+	glViewport(0, 0, glutWindowWidth, glutWindowHeight);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(fov, aspect, zNear, zFar);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(eyeX, eyeY, eyeZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+
+void display3D()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+	// Set up the Viewing Transformation (V matrix)	
+	gluLookAt(eyeX, eyeY, eyeZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+	drawGround();
+
+	loadModel();
+	loadedMesh->Draw();
+
+	glutSwapBuffers();
+}
+
+void drawGround()
+{
+	glPushMatrix();
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, groundMat_ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, groundMat_specular);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, groundMat_diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, groundMat_shininess);
+	glBegin(GL_QUADS);
+	glNormal3f(0, 1, 0);
+	glVertex3f(-22.0f, -4.0f, -22.0f);
+	glVertex3f(-22.0f, -4.0f, 22.0f);
+	glVertex3f(22.0f, -4.0f, 22.0f);
+	glVertex3f(22.0f, -4.0f, -22.0f);
+	glEnd();
+	glPopMatrix();
+}
+
+void loadModel()
+{
+	const aiScene* scene = importer.ReadFile("./ExportedMeshes/object.obj",
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices |
+		aiProcess_SortByPType);
+	
+	// load the mesh. We only have one mesh and it is in index 0
+	aiMesh* mesh = scene->mMeshes[0];
+
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+
+	// get data for vertices and normals
+	for (int i = 0; i < mesh->mNumVertices; i++) {
+		Vertex vertex;
+		Vector3D vector;
+		vertex.x = mesh->mVertices[i].x;
+		vertex.y = mesh->mVertices[i].y;
+		vertex.z = mesh->mVertices[i].z;
+
+		vector.x = mesh->mNormals[i].x;
+		vector.y = mesh->mNormals[i].y;
+		vector.z = mesh->mNormals[i].z;
+		vertex.normal = vector;
+
+		vertices.push_back(vertex);
+	}
+	
+	// get index data
+	for (int i = 0; i < mesh->mNumFaces; i++) {
+		aiFace face = mesh->mFaces[i];
+		for (int j = 0; j < face.mNumIndices; j++) { 
+			indices.push_back(face.mIndices[j]);
+		}
+	}
+
+	// get material data
+	if (mesh->mMaterialIndex >= 0 && scene->mMaterials) {
+		aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+		aiColor3D ambient(0.0f, 0.0f, 0.0f);
+		aiColor3D diffuse(0.0f, 0.0f, 0.0f);
+		aiColor3D specular(0.0f, 0.0f, 0.0f);
+		float shininess;
+
+		material->Get(AI_MATKEY_COLOR_AMBIENT, ambient);
+		material->Get(AI_MATKEY_COLOR_DIFFUSE, diffuse);
+		material->Get(AI_MATKEY_COLOR_SPECULAR, specular);
+		material->Get(AI_MATKEY_SHININESS, shininess);
+
+		GLfloat objectMat_ambient[] = { ambient.r, ambient.g, ambient.b, 1.0f };
+		GLfloat objectMat_diffuse[] = { diffuse.r, diffuse.g, diffuse.b, 1.0f };
+		GLfloat objectMat_specular[] = { specular.r, specular.g, specular.b, 1.0f };
+		GLfloat objectMat_shininess[] = { shininess };
+
+		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, objectMat_ambient);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, objectMat_diffuse);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, objectMat_specular);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, objectMat_shininess);
+	}
+
+	loadedMesh = new Mesh(vertices, indices);
+}
+
+int currentButton;
+
+void mouseScrollWheelHandler3D(int button, int dir, int xMouse, int yMouse)
+{
+	// Zoom in (scroll up)
+	if (dir > 0)
+	{
+		fov -= 5.0;
+		if (fov < 20.0) // Prevent excessive zoom-in
+			fov = 20.0;
+	}
+	// Zoom out (scroll down)
+	else
+	{
+		fov += 5.0;
+		if (fov > 120.0) // Prevent excessive zoom-out
+			fov = 120.0;
+	}
+
+	// Update the projection matrix to apply the new FOV
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(fov, aspect, zNear, zFar);
+	glMatrixMode(GL_MODELVIEW);
+
+	// Request a redraw
+	glutPostRedisplay();
+}
+
+float theta = 0.0f;
+float phi = 0.0f;
+void mouseMotionHandler3D(int x, int y)
+{
+	int dx = x - lastMouseX;
+	int dy = y - lastMouseY;
+	if (currentButton == GLUT_LEFT_BUTTON)
+	{
+		// update x and z positions
+		theta += dx * 0.01f;
+		eyeX = cos(theta) * radius;
+		eyeZ = sin(theta) * radius;
+	}
+	if (currentButton == GLUT_RIGHT_BUTTON)
+	{
+		phi += dy * 0.01f;
+		if (eyeY > 60) {
+			eyeY = 60;
+		}
+		else if (eyeY < 0) {
+			eyeY = 0;
+			phi = 0; // this seems to stop the camera from going into the ground
+		}
+		else {
+			eyeY = sin(phi) * radius;
+			
+		}
+	}
+	else if (currentButton == GLUT_MIDDLE_BUTTON)
+	{
+		// Fill in this code for zooming or ignore and use the scroll wheel
+	}
+	lastMouseX = x;
+	lastMouseY = y;
+	glutPostRedisplay();
+}
+
+void keyboardHandler3D(unsigned char key, int x, int y)
+{
+
+	switch (key)
+	{
+	case 'q':
+	case 'Q':
+	case 27:
+		// Esc, q, or Q key = Quit 
+		exit(0);
+		break;
+	default:
+		break;
+	}
+	glutPostRedisplay();
+}
